d2dcb4ac5c05f228d9410d1a10617a62b2738c52
Scott Giminiani <scottg489@gmail.com>
2025-11-28 17:04:59 -0500

    Fix publishing to maven central
    The old method of publishing no longer worked due to a number of backend
    changes. There are a lot of details, but this is a good starting point:
    
    https://central.sonatype.org/pages/ossrh-eol/
    
    In order to publish we needed to start using jreleaser which also
    involved upgrading gradle. We also are automatically publishing now
    which was a manual process before.

--- test.sh
+++ test.sh
@@ -10,14 +10,14 @@ cleanup() {
 
 declare ID_RSA_CONTENTS_BASE64
 # Assign actual credentials here
-declare OSSRH_USERNAME_BASE64="base64_username"
-declare OSSRH_PASSWORD_BASE64="base64_password"
+declare MAVENCENTRAL_USERNAME_BASE64="R2owMnFS"
+declare MAVENCENTRAL_PASSWORD_BASE64="MXNXY0JNa2V3ZnA2NHZWQW43a0xHd1BkWGJBWnpqTVda"
 
 # Change the location of these files based on where they are on your system
 ID_RSA_CONTENTS_BASE64=$(base64 ~/.ssh/id_rsa | tr -d '\n') ;
 [[ -n $ID_RSA_CONTENTS_BASE64 ]]
-[[ -n $OSSRH_USERNAME_BASE64 ]]
-[[ -n $OSSRH_PASSWORD_BASE64 ]]
+[[ -n $MAVENCENTRAL_USERNAME_BASE64 ]]
+[[ -n $MAVENCENTRAL_PASSWORD_BASE64 ]]
 
 # The local fs is mounted into the container and as such any files it writes will have their permissions changed.
 #   This will change the permissions back and clean up other files we don't want hanging around.
@@ -27,4 +27,4 @@ docker build infra/build -t rdglp-build-test && \
   docker run -it \
   --runtime=sysbox-runc \
   --volume "$PWD:/home/build-user/build/regex-directed-graph-line-parser" \
-  rdglp-build-test '{"ID_RSA": "'"$ID_RSA_CONTENTS_BASE64"'", "OSSRH_USERNAME": "'"$OSSRH_USERNAME_BASE64"'", "OSSRH_PASSWORD": "'"$OSSRH_PASSWORD_BASE64"'"}'
+  rdglp-build-test '{"ID_RSA": "'"$ID_RSA_CONTENTS_BASE64"'", "MAVENCENTRAL_USERNAME": "'"$MAVENCENTRAL_USERNAME_BASE64"'", "MAVENCENTRAL_PASSWORD": "'"$MAVENCENTRAL_PASSWORD_BASE64"'"}'
